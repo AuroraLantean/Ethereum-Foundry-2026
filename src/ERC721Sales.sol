b3e043f70188e15927cd4480268af5c7d72d7e38
AuroraLantean <raymondlieu.crypto@runbox.com>
2026-04-02 10:42:00 +0300

    add ERC721Sales and its test

---
+++ src/ERC721Sales.sol
@@ -0,0 +1,351 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
+
+// import
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol"; //includes IERC20
+//totalSupply(), tokenOfOwnerByIndex(address owner, uint256 index), tokenByIndex(uint256 index)
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; //includes IERC20, IERC20Permit, safeTransfer, safeTransferFrom, safeApprove, safeIncreaseAllowance, safeDecreaseAllowance
+import "@openzeppelin/contracts/utils/Address.sol";
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+//import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol"; //includes IERC721, which includes IERC165.sol
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol"; // includes IERC721
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol"; //includes IERC721Receiver
+
+//import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "forge-std/console.sol";
+
+//------------------------------==
+interface IERC721Full is IERC721Enumerable, IERC721Metadata {
+  function exists(
+    uint256 tokenId
+  ) external view returns (bool);
+} // use this instead of the full contract because we only need a few functions like transferFrom, safeTransferFrom, ownerOf, exists, etc...
+//----------------------==
+
+contract ERC721Sales is Ownable, ERC721Holder, ReentrancyGuard {
+  using SafeERC20 for IERC20Metadata;
+  using Address for address;
+  using Math for uint256;
+
+  IERC20Metadata public token;
+
+  struct NFT {
+    uint256 priceInWeiEth;
+    uint256 priceInWeiToken;
+  }
+
+  mapping(address nftAddr => mapping(uint256 nftId => NFT)) public prices;
+
+  constructor(
+    address erc20Addr
+  ) Ownable(msg.sender) {
+    require(erc20Addr != address(0), "should not be zero");
+    require(erc20Addr.code.length != 0, "should be contracts");
+    token = IERC20Metadata(erc20Addr);
+  }
+
+  function setToken(
+    address erc20Addr
+  ) external onlyOwner {
+    require(erc20Addr != address(0) && erc20Addr.code.length != 0, "input invalid");
+    token = IERC20Metadata(erc20Addr);
+  }
+
+  function setPriceBatchGuest(
+    address nftAddr,
+    uint256 minNftId
+  ) external {
+    uint8 decimals = token.decimals();
+    uint256 dpbase = 10 ** (uint256(decimals));
+    uint256[] memory ids = new uint256[](1);
+    ids[0] = 1e15;
+    _setPriceBatch(nftAddr, minNftId, minNftId, true, ids);
+    ids[0] = 11 * dpbase;
+    _setPriceBatch(nftAddr, minNftId + 1, minNftId + 1, false, ids);
+    ids[0] = 12 * dpbase;
+    _setPriceBatch(nftAddr, minNftId + 2, minNftId + 2, false, ids);
+  }
+
+  function setPriceBatch(
+    address nftAddr,
+    uint256 minNftId,
+    uint256 maxNftId,
+    bool isETH,
+    uint256[] memory priceArray
+  ) external onlyOwner {
+    _setPriceBatch(nftAddr, minNftId, maxNftId, isETH, priceArray);
+  }
+
+  function _setPriceBatch(
+    address nftAddr,
+    uint256 minNftId,
+    uint256 maxNftId,
+    bool isETH,
+    uint256[] memory priceArray
+  ) private {
+    require(maxNftId - minNftId + 1 == priceArray.length, "priceArray length invalid");
+
+    for (uint256 i = minNftId; i <= maxNftId; i++) {
+      uint256 price = priceArray[i - minNftId];
+      require(price > 0, "input invalid");
+      //console.log("id = %s,: %s", i, );
+      if (isETH) {
+        prices[nftAddr][i].priceInWeiEth = price;
+      } else {
+        prices[nftAddr][i].priceInWeiToken = price;
+      }
+    }
+  }
+
+  function buyNFTviaETH(
+    address nftAddr,
+    uint256 nftId
+  ) external payable {
+    IERC721Full erc721 = IERC721Full(nftAddr);
+    bool nftExisting = erc721.exists(nftId);
+    address owner = erc721.ownerOf(nftId);
+    require(nftExisting && msg.sender != owner, "invalid input");
+    uint256 priceInWeiEth = prices[nftAddr][nftId].priceInWeiEth;
+    require(priceInWeiEth > 0, "invalid price");
+    require(msg.value >= priceInWeiEth, "ETH amount invalid");
+    erc721.safeTransferFrom(owner, msg.sender, nftId);
+    emit BuyNFTViaETH(msg.sender, nftId, msg.value, address(this).balance);
+  }
+
+  function buyNFTviaERC20(
+    address nftAddr,
+    uint256 nftId
+  ) external {
+    IERC721Full erc721 = IERC721Full(nftAddr);
+    bool nftExisting = erc721.exists(nftId);
+    address owner = erc721.ownerOf(nftId);
+    require(nftExisting && msg.sender != owner, "invalid input");
+    uint256 priceInWeiToken = prices[nftAddr][nftId].priceInWeiToken;
+    require(priceInWeiToken > 0, "invalid price");
+    token.safeTransferFrom(msg.sender, address(this), priceInWeiToken);
+
+    erc721.safeTransferFrom(owner, msg.sender, nftId);
+    emit BuyNFTViaERC20(msg.sender, nftId, priceInWeiToken, address(this).balance);
+  }
+
+  function withdrawNFT(
+    address nftAddr,
+    address _to,
+    uint256 minNftId,
+    uint256 maxNftId
+  ) external onlyOwner {
+    require(_to != address(0) && _to != address(this), "to address invalid");
+    require(msg.sender != address(this), "sender invalid");
+
+    IERC721Full erc721 = IERC721Full(nftAddr);
+
+    for (uint256 tokenId = minNftId; tokenId <= maxNftId; tokenId++) {
+      address owner = erc721.ownerOf(tokenId);
+      if (erc721.exists(tokenId) && owner == address(this)) {
+        erc721.safeTransferFrom(address(this), _to, tokenId, "");
+      }
+    }
+    emit WithdrawNFT(_to, minNftId, maxNftId);
+  }
+
+  function sellNFTviaETH(
+    address nftAddr,
+    uint256 nftId
+  ) external nonReentrant {
+    IERC721Full erc721 = IERC721Full(nftAddr);
+    bool nftExisting = erc721.exists(nftId);
+    address owner = erc721.ownerOf(nftId);
+    require(nftExisting && msg.sender == owner, "invalid input");
+
+    erc721.safeTransferFrom(msg.sender, address(this), nftId);
+
+    uint256 priceInWeiEth = prices[nftAddr][nftId].priceInWeiEth.mulDiv(9, 10);
+    require(priceInWeiEth > 0, "invalid price");
+    Address.sendValue(payable(msg.sender), priceInWeiEth); // also check this ctrt ETH balance >= amount
+    emit SellNFTViaETH(payable(msg.sender), nftId, priceInWeiEth, address(this).balance);
+  }
+
+  function sellNFTviaERC20(
+    address nftAddr,
+    uint256 nftId
+  ) external {
+    IERC721Full erc721 = IERC721Full(nftAddr);
+    bool nftExisting = erc721.exists(nftId);
+    address owner = erc721.ownerOf(nftId);
+    require(nftExisting && msg.sender == owner, "invalid input");
+
+    erc721.safeTransferFrom(msg.sender, address(this), nftId);
+
+    uint256 priceInWeiToken = prices[nftAddr][nftId].priceInWeiToken;
+    require(priceInWeiToken > 0, "invalid price");
+    uint256 amount = priceInWeiToken.mulDiv(9, 10);
+    token.safeTransfer(msg.sender, amount);
+
+    emit SellNFTViaERC20(msg.sender, nftId, amount, address(this).balance);
+  }
+
+  function withdrawETH(
+    address payable _to,
+    uint256 _amount
+  ) external onlyOwner nonReentrant {
+    require(_to != address(0) && _to != address(this), "to address invalid");
+    require(_amount > 0, "amount invalid");
+    Address.sendValue(_to, _amount); //check this ctrt ETH balance >= amount
+    //payable(address(_to)).transfer(_amount);
+    emit WithdrawETH(_to, _amount, address(this).balance);
+  }
+
+  function withdrawERC20(
+    address _to,
+    uint256 _amount
+  ) external onlyOwner {
+    require(_to != address(0) && _to != address(this), "to address invalid");
+    require(_amount > 0 && _amount <= token.balanceOf(address(this)), "amount invalid");
+    token.safeTransfer(_to, _amount);
+    emit WithdrawERC20(_to, _amount, token.balanceOf(address(this)));
+  }
+
+  fallback() external payable { }
+
+  receive() external payable {
+    if (msg.value > 0) {
+      revert();
+    }
+  }
+
+  event WithdrawETH(address indexed payee, uint256 amount, uint256 balance);
+  event WithdrawERC20(address indexed payee, uint256 amount, uint256 balance);
+  event BuyNFTViaETH(address indexed payer, uint256 indexed tokenId, uint256 amount, uint256 balance);
+  event SellNFTViaETH(address payable indexed seller, uint256 indexed tokenId, uint256 amount, uint256 balance);
+  event WithdrawNFT(address indexed to, uint256 indexed minNftId, uint256 maxNftId);
+  event BuyNFTViaERC20(address indexed payer, uint256 indexed tokenId, uint256 amount, uint256 balance);
+  event SellNFTViaERC20(address indexed seller, uint256 indexed tokenId, uint256 amount, uint256 balance);
+
+  //---------------==
+  function getData(
+    address nftAddr,
+    uint256 nftId
+  ) public view returns (uint256, uint256, address) {
+    return (prices[nftAddr][nftId].priceInWeiEth, prices[nftAddr][nftId].priceInWeiToken, address(token));
+  }
+
+  function getBalances(
+    address erc20Addr,
+    address nftAddr
+  ) public view returns (uint256[] memory out) {
+    address sender = msg.sender;
+    IERC20Metadata token1 = IERC20Metadata(erc20Addr);
+    IERC721Full erc721c = IERC721Full(nftAddr);
+    address tis = address(this);
+    out = new uint256[](7);
+    out[0] = sender.balance;
+    out[1] = token1.balanceOf(sender);
+    out[2] = uint256(token1.decimals());
+    out[3] = erc721c.balanceOf(sender);
+    out[4] = tis.balance;
+    out[5] = token1.balanceOf(tis);
+    out[6] = erc721c.balanceOf(tis);
+  }
+  /*     function getBalances() public view returns (uint256[] memory out) {
+      address sender = msg.sender;
+      address tis = address(this);
+      out = new uint256[](7);
+      out[0] = sender.balance;
+      out[1] = token.balanceOf(sender);
+      out[2] = uint256(token.decimals());
+      out[3] = erc721.balanceOf(sender);
+      out[4] = tis.balance;
+      out[5] = token.balanceOf(tis);
+      out[6] = erc721.balanceOf(tis);
+  } */
+}
+
+contract ArrayOfStructs {
+  struct Box {
+    uint256 num;
+    address owner;
+  }
+
+  mapping(uint256 => Box) public boxes;
+
+  constructor(
+    uint256 num
+  ) {
+    address owner = msg.sender;
+    boxes[0] = Box(num, owner);
+    boxes[1] = Box(num + 1, owner);
+    boxes[2] = Box(num + 2, owner);
+  }
+
+  function addBox(
+    uint256 id,
+    uint256 num,
+    address owner
+  ) public returns (bool) {
+    boxes[id] = Box(num, owner);
+    return true;
+  }
+
+  function getBox(
+    uint256 id
+  ) public view returns (Box memory) {
+    return boxes[id];
+  }
+
+  function getBox2(
+    uint256 id
+  ) public view returns (Box memory box) {
+    box = boxes[id];
+  }
+
+  function getTuple() public pure returns (string memory, uint8, uint256) {
+    return ("string", type(uint8).max, type(uint256).max);
+  }
+
+  function getBoxes(
+    uint256 min,
+    uint256 max
+  ) public view returns (Box[] memory) {
+    uint256 arrlength = max - min + 1;
+    Box[] memory out = new Box[](arrlength);
+    for (uint256 i = min; i < arrlength; i++) {
+      out[i] = boxes[i];
+    }
+    return out;
+  }
+
+  function getBoxes2(
+    uint256 min,
+    uint256 max
+  ) public view returns (Box[] memory out) {
+    uint256 arrlength = max - min + 1;
+    out = new Box[](arrlength);
+    for (uint256 i = 0; i < arrlength; i++) {
+      out[i] = boxes[i];
+    }
+  }
+
+  function getBalances(
+    address erc20Addr,
+    address nftAddr
+  ) public view returns (uint256[] memory out) {
+    address sender = msg.sender;
+    IERC20Metadata token = IERC20Metadata(erc20Addr);
+    IERC721Full erc721 = IERC721Full(nftAddr);
+    address tis = address(this);
+    out = new uint256[](7);
+    out[0] = sender.balance;
+    out[1] = token.balanceOf(sender);
+    out[2] = uint256(token.decimals());
+    out[3] = erc721.balanceOf(sender);
+    out[4] = tis.balance;
+    out[5] = token.balanceOf(tis);
+    out[6] = erc721.balanceOf(tis);
+  }
+} //ArrayOfStructs
---
+++ test/ERC721Sales.t.sol
@@ -0,0 +1,332 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
+
+import "forge-std/Test.sol";
+import "src/ERC20Token.sol";
+import "src/ERC721Token.sol";
+import "src/ERC721Sales.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract ERC721SalesTest is Test, ERC721Holder {
+  address public zero = address(0);
+  address public alice = address(1);
+  address public bob = address(2);
+  address public eve = address(5);
+  address public tis = address(this);
+  ERC721Sales public sales;
+  ERC721Token public dragons;
+  USDX public usdt;
+  ERC20Token public token0;
+  ArrayOfStructs public ctrt;
+  address public owner;
+  address public nftAddr;
+  address public salesAddr;
+  address public tokenAddr;
+  uint256 public aGenBf;
+  uint256 public aGenAf;
+  uint256 public aNftBf;
+  uint256 public aNftAf;
+  uint256 public cGenBf;
+  uint256 public cGenAf;
+  uint256 public cNftBf;
+  uint256 public cNftAf;
+  uint256 public amount = 1000;
+  uint256 public priceInWeiEth = 1e15;
+  uint256 public tokenDp = 1e6;
+  uint256 public priceInWeiToken = 100 * tokenDp;
+  uint256 public minNftId = 0;
+  uint256 public maxNftId = 9;
+  bool public boo = false;
+  uint256[] public uints;
+  //IWETH_dup public weth;
+
+  receive() external payable {
+    console.log("ETH received from:", msg.sender);
+    console.log("ETH received in Szabo:", msg.value / 1e12);
+  }
+
+  function setUp() external {
+    console.log("---------== Setup()");
+    deal(alice, 1 ether);
+    deal(bob, 1 ether);
+    aGenBf = alice.balance;
+    console.log("Alice ETH:", aGenBf);
+    aGenBf = bob.balance;
+    console.log("Bob ETH:", aGenBf);
+
+    //USDT, USDC use 6 dp !!! But DAI has 18!!
+    usdt = new USDX("TetherUSD", "USDT");
+    dragons = new ERC721Token("DragonsNFT", "DRAG", minNftId, maxNftId);
+    nftAddr = address(dragons);
+    console.log("nftAddr", nftAddr);
+    usdt.mint(alice, 1000e6);
+    aGenBf = usdt.balanceOf(alice);
+    console.log("Alice USDT:", aGenBf / 1e6, aGenBf);
+    assertEq(aGenBf, 1000e6);
+    tokenAddr = address(usdt);
+    console.log("tokenAddr", tokenAddr);
+    sales = new ERC721Sales(tokenAddr);
+    salesAddr = address(sales);
+    console.log("salesAddr", salesAddr);
+
+    aNftBf = dragons.balanceOf(tis);
+    console.log("aNftBf tis:", aNftBf);
+  }
+
+  function _balc(
+    address user,
+    string memory userName,
+    address tokenCtrt,
+    string memory nameTheOther
+  ) private view returns (uint256 uTok, uint256 uNft, uint256 cTok, uint256 cNft) {
+    address theOther = salesAddr;
+    if (tokenCtrt == zero) {
+      cTok = theOther.balance;
+      cNft = dragons.balanceOf(theOther);
+      console.log("%s ETH in Szabo: %s, NFT: %s", nameTheOther, cTok / 1e12, cNft);
+
+      uTok = user.balance;
+      uNft = dragons.balanceOf(user);
+      console.log("%s ETH in Szabo: %s, NFT: %s", userName, uTok / 1e12, uNft);
+    } else {
+      cTok = usdt.balanceOf(theOther);
+      cNft = dragons.balanceOf(theOther);
+      console.log("%s usdt: %s, NFT: %s", nameTheOther, cTok / tokenDp, cNft);
+
+      uTok = usdt.balanceOf(user);
+      uNft = dragons.balanceOf(user);
+      console.log("%s usdt: %s, NFT: %s", userName, uTok / tokenDp, uNft);
+    }
+  }
+
+  function testInit() external {
+    console.log("--------== testInit");
+    console.log("safeApproveBatch...");
+    dragons.safeApproveBatch(salesAddr, 0, 9);
+
+    for (uint256 i = 0; i <= 9; i++) {
+      address approvedOptr = dragons.getApproved(i);
+      //console.log("approved operator: ", approvedOptr);
+      assertEq(approvedOptr, salesAddr);
+    }
+
+    console.log("--------== buyNFTviaERC20");
+    (aGenBf, aNftBf, cGenBf, cNftBf) = _balc(alice, "Alice", tokenAddr, "SalesCtrt");
+    uints.push(priceInWeiToken);
+    //new uint256[](priceInWeiToken)
+    sales.setPriceBatch(nftAddr, 0, 0, false, uints);
+    vm.startPrank(alice);
+    usdt.approve(salesAddr, priceInWeiToken);
+    sales.buyNFTviaERC20(nftAddr, 0);
+    vm.stopPrank();
+    (aGenAf, aNftAf, cGenAf, cNftAf) = _balc(alice, "Alice", tokenAddr, "SalesCtrt");
+    assertEq(aGenBf - aGenAf, priceInWeiToken);
+    assertEq(aNftAf - aNftBf, 1);
+    assertEq(cGenAf - cGenBf, priceInWeiToken);
+
+    uint256[] memory out = sales.getBalances(tokenAddr, nftAddr);
+    console.log("out:", out[0], out[1], out[2]);
+
+    console.log("--------== withdrawERC20");
+    (aGenBf, aNftBf, cGenBf, cNftBf) = _balc(tis, "tis", tokenAddr, "SalesCtrt");
+    sales.withdrawERC20(tis, usdt.balanceOf(salesAddr));
+    (aGenAf, aNftAf, cGenAf, cNftAf) = _balc(tis, "tis", tokenAddr, "SalesCtrt");
+    assertEq(aGenAf, aGenBf + priceInWeiToken);
+
+    console.log("--------== BuyNFTviaETH");
+    (aGenBf, aNftBf, cGenBf, cNftBf) = _balc(alice, "Alice", zero, "SalesCtrt");
+    uints.pop();
+    uints.push(priceInWeiEth);
+    sales.setPriceBatch(nftAddr, 1, 1, true, uints);
+    vm.startPrank(alice);
+    sales.buyNFTviaETH{ value: priceInWeiEth }(nftAddr, 1);
+    vm.stopPrank();
+    (aGenAf, aNftAf, cGenAf, cNftAf) = _balc(alice, "Alice", zero, "SalesCtrt");
+    assertEq(aGenBf - aGenAf, priceInWeiEth);
+    assertEq(aNftAf - aNftBf, 1);
+    assertEq(cGenAf - cGenBf, priceInWeiEth);
+
+    console.log("--------== withdrawETH");
+    (aGenBf, aNftBf, cGenBf, cNftBf) = _balc(tis, "tis", zero, "SalesCtrt");
+    sales.withdrawETH(payable(tis), cGenAf);
+    console.log("after withdrawETH");
+    (aGenAf, aNftAf, cGenAf, cNftAf) = _balc(tis, "tis", zero, "SalesCtrt");
+    assertEq(cGenAf, 0);
+    assertEq(aGenAf - aGenBf, priceInWeiEth);
+  }
+
+  function testWithdrawNFT() external {
+    console.log("----== testWithdrawNFT");
+    dragons.safeTransferFromBatch(tis, salesAddr, 0, 9);
+    aNftBf = dragons.balanceOf(salesAddr);
+    console.log("aNftBf salesAddr:", aNftBf);
+    assertEq(aNftBf, 10);
+
+    aNftBf = dragons.balanceOf(tis);
+    console.log("aNftBf tis:", aNftBf);
+    assertEq(aNftBf, 0);
+
+    sales.withdrawNFT(nftAddr, tis, 0, 9);
+    aNftBf = dragons.balanceOf(salesAddr);
+    console.log("aNftBf salesAddr:", aNftBf);
+    assertEq(aNftBf, 0);
+
+    aNftBf = dragons.balanceOf(tis);
+    console.log("aNftBf tis:", aNftBf);
+    assertEq(aNftBf, 10);
+  }
+
+  function testSellNFTviaETH() external {
+    console.log("----== testSellNFTviaETH");
+    aNftBf = dragons.balanceOf(tis);
+    console.log("aNftBf tis:", aNftBf);
+    assertEq(aNftBf, 10);
+    console.log("tis ETH: %s", tis.balance / 1e18);
+
+    deal(salesAddr, 1000 ether);
+    console.log("SalesCtrt ETH: %s", salesAddr.balance / 1e18, salesAddr.balance);
+
+    uints.push(1e15);
+    sales.setPriceBatch(nftAddr, 0, 0, true, uints);
+    uints.pop();
+    (uint256 nftPriceEth,) = sales.prices(nftAddr, 0);
+    console.log("nftPriceEth in 1e15:", nftPriceEth / 1e15);
+
+    dragons.approve(salesAddr, 0);
+    sales.sellNFTviaETH(nftAddr, 0);
+    console.log("tis ETH: %s", tis.balance / 1e18);
+    console.log("SalesCtrt ETH: %s", salesAddr.balance / 1e18, salesAddr.balance);
+
+    //Custom Error
+    bytes4 errorSignature = 0xcd786059;
+    bytes4 desiredSelector = bytes4(keccak256(bytes("AddressInsufficientBalance(address)")));
+    console.log(errorSignature == desiredSelector);
+  }
+
+  function testSellNFTviaERC20() external {
+    console.log("----== testSellNFTviaERC20");
+    aNftBf = dragons.balanceOf(salesAddr);
+    console.log("aNftBf SalesCtrt:", aNftBf);
+
+    aGenBf = usdt.balanceOf(salesAddr);
+    console.log("SalesCtrt USDT: %s", aGenBf / 1e6);
+    amount = 1000;
+    boo = usdt.transfer(salesAddr, amount * 1e6);
+    assert(boo);
+    aGenAf = usdt.balanceOf(salesAddr);
+    console.log("SalesCtrt USDT: %s", aGenAf / 1e6);
+
+    aGenBf = usdt.balanceOf(tis);
+    console.log("tis USDT: %s", aGenBf / 1e6);
+
+    dragons.approve(salesAddr, 0);
+    console.log("after approve()");
+
+    uints.push(100e6);
+    sales.setPriceBatch(nftAddr, 0, 0, false, uints);
+    uints.pop();
+    (, uint256 nftPriceUSDT) = sales.prices(nftAddr, 0);
+    console.log("nftPriceUSDT:", nftPriceUSDT / 1e6);
+    sales.sellNFTviaERC20(nftAddr, 0);
+
+    aGenBf = usdt.balanceOf(tis);
+    console.log("tis USDT: %s", aGenBf / 1e6);
+    aGenAf = usdt.balanceOf(salesAddr);
+    console.log("SalesCtrt USDT: %s", aGenAf / 1e6);
+
+    aNftAf = dragons.balanceOf(salesAddr);
+    console.log("aNftAf SalesCtrt:", aNftAf);
+  }
+
+  function testSetPriceBatch() public {
+    console.log("----== testSetPriceBatch");
+    for (uint256 i = minNftId; i <= maxNftId; i++) {
+      uints.push((1 + i) * 1e15);
+      sales.setPriceBatch(nftAddr, i, i, true, uints);
+      uints.pop();
+      uints.push((100 + i) * 1e6);
+      sales.setPriceBatch(nftAddr, i, i, false, uints);
+      uints.pop();
+      (uint256 priceEth, uint256 priceTok) = sales.prices(nftAddr, i);
+      console.log(priceEth, priceTok);
+      //1000000000000000 100000000
+    }
+  }
+
+  function testzGuestMintSetPriceBuy() public {
+    console.log("----== testGuestMintSetPriceBuy");
+    minNftId = 100;
+    vm.startPrank(alice);
+    dragons.safeMintToGuest(salesAddr, minNftId);
+
+    aNftBf = dragons.balanceOf(salesAddr);
+    assertEq(aNftBf, 3);
+    owner = dragons.ownerOf(minNftId);
+    console.log("owner", owner);
+    assertEq(owner, salesAddr);
+
+    sales.setPriceBatchGuest(nftAddr, minNftId);
+    for (uint256 i = minNftId; i <= minNftId + 2; i++) {
+      (uint256 priceEth, uint256 priceTok) = sales.prices(nftAddr, i);
+      console.log(priceEth, priceTok);
+    }
+    console.log("-----== buyNFTviaETH");
+    console.log("ETH balc of Alice in 1e15: %s", alice.balance / 1e15);
+    console.log("ETH balc of SalesCtrt in 1e15: %s", salesAddr.balance / 1e15, salesAddr.balance);
+
+    sales.buyNFTviaETH{ value: 1e15 }(nftAddr, minNftId);
+    console.log("ETH balc of Alice in 1e15: %s", alice.balance / 1e15);
+    console.log("ETH balc of SalesCtrt in 1e15: %s", salesAddr.balance / 1e15, salesAddr.balance);
+    aNftBf = dragons.balanceOf(alice);
+    console.log("aNftBf alice:", aNftBf);
+
+    //usdt
+    console.log("-----== buyNFTviaERC20");
+    aGenBf = usdt.balanceOf(alice);
+    console.log("aGenBf alice:", aGenBf / 1e6, aGenBf);
+
+    usdt.approve(salesAddr, 100e6);
+    console.log("here020");
+    sales.buyNFTviaERC20(nftAddr, minNftId + 1);
+
+    aNftBf = dragons.balanceOf(alice);
+    console.log("aNftBf alice:", aNftBf);
+
+    aGenAf = usdt.balanceOf(alice);
+    console.log("aGenAf alice:", aGenAf / 1e6, aGenAf);
+    vm.stopPrank();
+  }
+
+  function testArrayOfStructs() public {
+    console.log("----== ArrayOfStructs");
+    ctrt = new ArrayOfStructs(100);
+
+    uint256 id = 0;
+    ArrayOfStructs.Box memory box;
+    box = ctrt.getBox(id);
+    console.log("getBox:", id, box.num, box.owner);
+    box = ctrt.getBox2(id);
+    console.log("getBox2:", id, box.num, box.owner);
+
+    id = 3;
+    bool oBool = ctrt.addBox(id, 100 + id, tis);
+    console.log("oBool:", oBool);
+    box = ctrt.getBox(id);
+    console.log("getBox:", id, box.num, box.owner);
+
+    ArrayOfStructs.Box[] memory boxes;
+    boxes = ctrt.getBoxes(0, 3);
+    console.log("getBoxes. out length:", boxes.length);
+    console.log(boxes[0].num, boxes[1].num, boxes[2].num, boxes[3].num);
+
+    boxes = ctrt.getBoxes2(0, 3);
+    console.log("getBoxes. out length:", boxes.length);
+    console.log(boxes[0].num, boxes[1].num, boxes[2].num, boxes[3].num);
+
+    uint256[] memory uintsO;
+    uintsO = ctrt.getBalances(tokenAddr, nftAddr);
+    console.log("getBalances:");
+    console.log(uintsO[0], uintsO[1], uintsO[2], uintsO[3]);
+    console.log(uintsO[4], uintsO[5], uintsO[6]);
+  }
+}
